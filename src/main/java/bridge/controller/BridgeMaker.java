0370c4df3154c6655444a87447db84e9531ac79b
ohhyungsuh <ohhyungsuh@gmail.com>
2022-11-18 02:36:31 +0900

    feat: BridgeMaer 기능 구현

--- src/main/java/bridge/controller/BridgeMaker.java
+++ src/main/java/bridge/controller/BridgeMaker.java
@@ -1,7 +1,9 @@
 package bridge.controller;
 
 import bridge.BridgeNumberGenerator;
+import bridge.BridgeRandomNumberGenerator;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -9,7 +11,7 @@ import java.util.List;
  */
 public class BridgeMaker {
 
-    private final BridgeNumberGenerator bridgeNumberGenerator;
+    private final BridgeNumberGenerator bridgeNumberGenerator
 
     public BridgeMaker(BridgeNumberGenerator bridgeNumberGenerator) {
         this.bridgeNumberGenerator = bridgeNumberGenerator;
@@ -20,6 +22,11 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        return null;
+        String[] bridgeArr = {"D", "U"};
+        List<String> bridge = new ArrayList<>();
+        for(int i=0; i<size; i++){
+            bridge.add(bridgeArr[bridgeNumberGenerator.generate()]);
+        }
+        return bridge;
     }
 }
